dd70d0d60cea918ae304e9e7fcd9853dce66c12c
zhuaigen.chen <103731829+jiaxinchen-max@users.noreply.github.com>
2025-09-02 17:32:44 +0000

    termuxdc

--- x11-packages/termux-display-client/build.sh
+++ x11-packages/termux-display-client/build.sh
@@ -5,17 +5,16 @@ TERMUX_PKG_MAINTAINER="@chenjiaxin-max"
 TERMUX_PKG_VERSION=(0.0.1
                     21.1.8)
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=(http://172.17.0.2:8081/termux-packages/termux-display-client.tar.gz
+TERMUX_PKG_SRCURL=(http://127.0.0.1:8081/termux-packages/termux-display-client.tar.gz
                    https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz)
-TERMUX_PKG_SHA256=(eaef1815aaa842d74db9bf9ade76069ce24f31ad7a25711a0389ea78a68002d5
+TERMUX_PKG_SHA256=(0ec3434e2829c0af2263f3bc91a05b3aad7e01df1822ca6b3db2564373faf44a
                    38aadb735650c8024ee25211c190bf8aad844c5f59632761ab1ef4c4d5aeb152)
-TERMUX_PKG_DEPENDS="libpixman, libx11"
-TERMUX_PKG_BUILD_DEPENDS="xorg-server-xvfb"
+# TERMUX_PKG_BUILD_DEPENDS="xorg-server-xvfb"
 TERMUX_PKG_API_LEVEL="26"
-TERMUX_PKG_DEPENDS="libpixman, libx11, xorg-server-xvfb"
+TERMUX_PKG_DEPENDS="libpixman, libx11"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BUILD_IN_SRC=false
 
 termux_step_post_get_source() {
 	## TigerVNC requires sources of X server (either Xorg or Xvfb).
